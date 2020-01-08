8a390c4f7d9254bcd7a8dfd948e95f97a0c76b7e
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-01-07 21:05:04 -0600

    CORS dependency added"

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 
+
+const cors = require('cors');
+
 const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
+
+server.use(cors({
+    origin: "https://chef-portfolio-11.firebaseapp.com/",
+}));
+
+
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
